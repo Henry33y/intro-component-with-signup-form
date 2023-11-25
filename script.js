885ee8db5f0dcedac03bf089b34d7164f3c0a28f
Henry <henryasante336@gmail.com>
2023-11-25 17:23:05 +0000

    Finished Frontend work

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const form = document.querySelector('form')
 const inputs = document.querySelectorAll('.formInput')
 const submitBtn = document.querySelector('.submitBtn')
 
+// console.log(inputs);
+console.log(inputs[0].nextElementSibling.nextElementSibling);
+
+submitBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    for(let i = 0;i < inputs.length;i++){
+        if(inputs[i].value == ''){
+            inputs[i].nextElementSibling.style.display = 'flex'
+            inputs[i].nextElementSibling.nextElementSibling.style.display = 'flex'
+        }
+        else{
+            inputs[i].nextElementSibling.style.display = 'none'
+            inputs[i].nextElementSibling.nextElementSibling.style.display = 'none'
+        }
+    }
+    
+    console.log(inputs[3].value.length);
+})
